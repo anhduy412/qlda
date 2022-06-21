5b0be439307f272ebf42431644188966d13d8a1d
Kinghood15 <annguyen8278163@gmail.com>
2022-06-22 06:09:15 +0700

    Update file profile

--- static/assets/js/profile.js
+++ static/assets/js/profile.js
@@ -1,29 +1,31 @@
 const btnEditProfile = document.querySelector('#btn-edit-profile');
 const inputUsername = document.querySelector('#input-username');
 const inputEmail = document.querySelector('#input-email');
-const inputFirstName = document.querySelector('#input-first-name');
-const inputLastName = document.querySelector('#input-last-name');
-const inputTeam = document.querySelector('#input-team');
-const inputPosition = document.querySelector('#input-position');
+const inputFullName = document.querySelector('#input-fullname');
+const inputPhoneNumber = document.querySelector('#input-phonenumber');
+const inputGender = document.querySelector('#gender');
+const inputTeam = document.querySelector('#team');
+const inputPosition = document.querySelector('#position');
 // const btnSaveEditProfile = document.querySelector('#btn-save-edit-profile');
 const btnEditProfileDiv = document.querySelector('.btn-edit-profile-div-footer');
 
 btnEditProfile.addEventListener('click',() =>{
     inputUsername.readOnly = true;
     inputEmail.readOnly = false;
-    inputFirstName.readOnly = false;
-    inputLastName.readOnly = false;
-    inputTeam.readOnly = false;
-    inputPosition.readOnly = false;
-    console.log(btnEditProfileDiv)
+    inputFullName.readOnly = false;
+    inputPhoneNumber.readOnly = false;
+    inputTeam.disabled = false;
+    inputPosition.disabled = false;
+    inputGender.disabled = false;
     btnEditProfileDiv.innerHTML = '<button id="btn-save-edit-profile" class="btn btn-neutral">Lưu sự thay đổi </button>';
 })
 btnSaveEditProfile.addEventListener('click',() => {
     inputUsername.readOnly = true;
     inputEmail.readOnly = true;
-    inputFirstName.readOnly = true;
-    inputLastName.readOnly = true;
-    inputTeam.readOnly = true;
-    inputPosition.readOnly = true;
+    inputFullName.readOnly = true;
+    inputPhoneNumber.readOnly = true;
+    inputTeam.disabled = true;
+    inputPosition.disabled = true;
+    inputGender.disabled = true;
     btnSaveEditProfile.remove();
 })
